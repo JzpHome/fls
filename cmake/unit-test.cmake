fcf12fb11ab7ceda50da28e0d3432bc796479180
JZPeng <jzpe2.718@hotmail.com>
2022-06-26 20:49:40 +0800

    [fls] fix cmake error when others use fls by FetchContent

--- cmake/unit-test.cmake
+++ cmake/unit-test.cmake
@@ -1,17 +1,21 @@
-set(TEST_SOURCE_DIR ${PROJECT_HOME_DIR}/test-dir)
-set(TEST_SOURCES    ${TEST_SOURCE_DIR}/main.cc
-                    ${TEST_SOURCE_DIR}/fls.cc)
+set(FLS_TEST_SOURCES    test-dir/main.cc
+                        test-dir/fls.cc)
 
 
 
 #
-add_executable(unit_test ${TEST_SOURCES})
-target_link_libraries(unit_test fls gtest_main)
-target_include_directories(unit_test PRIVATE ${PROJECT_HEADER_DIR})
+add_executable(fls_test ${FLS_TEST_SOURCES})
+target_link_libraries(fls_test fls gtest_main)
+target_include_directories(fls_test PRIVATE include)
+set_target_properties(fls_test
+    PROPERTIES
+    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin"
+    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib"
+    ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib")
 
 
 
 #
 enable_testing()
 include(GoogleTest)
-gtest_discover_tests(unit_test)
+gtest_discover_tests(fls_test)
